2d6fc2642a4e5adc5b9d095ecb52037776d06587
Decode Marines <goipukraine@gmail.com>
2023-09-15 11:36:08 +0100

    Set1

--- ex.hs
+++ ex.hs
@@ -4,11 +4,6 @@ main :: IO ()
 main = do
     print "hello"
 
-
-data Tree a = Node a (Tree a) (Tree a) | Empty
-
--- example :: Tree Int
--- example = (Node 0 (Node 1 (Node 2 Empty Empty)
---                           (Node 3 Empty Empty))
---                   (Node 4 Empty Empty))
-
+ilog3 :: Integer -> Integer
+ilog3 0 = 0
+ilog3 n = 1 + ilog3 (div n 3)
--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -156,4 +156,5 @@ power n k = n * power n (k - 1)
 --   ilog3 7 ==> 2
 
 ilog3 :: Integer -> Integer
-
+ilog3 0 = 0
+ilog3 n = 1 + ilog3 (div n 3)
